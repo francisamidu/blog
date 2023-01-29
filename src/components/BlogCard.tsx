0c7061117cd221c4679016f6be1b1a485876704b
francisamidu <francisamidu124@gmail.com>
2023-01-29 10:16:55 +0200

    chore:Blog card styling

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,27 +14,17 @@ const BlogCard = ({
       <p className="my-4 text-new-200">
         {excerpt.length > 175 ? `${excerpt.slice(0, 175)}....` : excerpt}
       </p>
-      <div className="flex flex-row items-center justify-between">
-        {newTags.map((tag, index) =>
-          newTags.length - index !== 1 ? (
-            <>
-              <span
-                className="uppercase text-xs font-bold text-new-400 bg-gray-100 py-0.5 px-2 rounded"
-                key={index}
-              >
-                {tag}
-              </span>
-              <span className="mx-3 text-new-400">-</span>
-            </>
-          ) : (
+      <div className="flex flex-row items-center justify-start">
+        {newTags.map((tag, index) => (
+          <>
             <span
-              className="uppercase text-xs font-bold text-new-400 bg-gray-100 py-0.5 px-2 rounded"
+              className="uppercase text-xs font-bold text-new-100 bg-blue-200 py-1 px-3 rounded mr-2 hover:cursor-pointer"
               key={index}
             >
               {tag}
             </span>
-          )
-        )}
+          </>
+        ))}
       </div>
       <p className="my-4 text-new-100 hover:cursor-pointer hover:text-new-400 transition-all duration-200">
         Read more
