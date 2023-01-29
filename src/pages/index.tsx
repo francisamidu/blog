2976fdddc342ab4adab7a631bb7c9d924efbec7c
francisamidu <francisamidu124@gmail.com>
2023-01-29 09:46:04 +0200

    UI tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,20 +13,27 @@ export default function Blog({ posts }: BlogProps) {
       <Head>
         <title>Francis Amidu{`'`}s blog</title>
       </Head>
-      <main className="bg-gray-100 max-w-screen-lg md:mx-auto">
-        <div className="p-10 w-full bg-white">
-          <h1 className="font-bold text-4xl text-new-100">
-            Welcome to my blog
-          </h1>
-          <p className="text-new-200 text-2xl font-light my-4">
-            Here i detail my journey as a software engineer. I write about the
-            projects i{`'`}ve worked on, the bugs i have encountered and solved
-            and everything i learned.
-          </p>
+      <main className="bg-white max-w-screen-lg md:mx-auto">
+        <div className="border-b border-b-gray-100">
+          <div className="p-10 w-full">
+            <h1 className="font-bold text-4xl text-new-100">
+              Welcome to my blog
+            </h1>
+            <p className="text-new-200 text-2xl font-light my-4">
+              Here i detail my journey as a software engineer. I write about the
+              projects i{`'`}ve worked on, the bugs i have encountered and
+              solved and everything i learned.
+            </p>
+          </div>
         </div>
-        {posts.map((frontmatter) => (
-          <BlogCard key={frontmatter.slug} data={frontmatter} />
-        ))}
+        <div className="p-2">
+          <h2 className="uppercase text-xs font-bold text-new-300">
+            Recently published
+          </h2>
+          {posts.map((frontmatter) => (
+            <BlogCard key={frontmatter.slug} data={frontmatter} />
+          ))}
+        </div>
       </main>
     </>
   );
